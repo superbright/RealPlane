1768d1c417c05560c314cd6dc39592fc4e995d67
igal <inassima@gmail.com>
2016-10-09 14:45:33 -0500

    Rotation between map and scanner
    In Plane Object under vuforia tracker, the rotation data is used to
    change between objects

--- Assets/Scripts/AngleTracker.cs
+++ Assets/Scripts/AngleTracker.cs
@@ -6,7 +6,7 @@ public class AngleTracker : MonoBehaviour {
 
     public DefaultTrackableEventHandler tracker;
     public ImageTargetBehaviour trackingmarker;
-    public UnityEngine.UI.Text display1;
+   // public UnityEngine.UI.Text display1;
     public GameObject scanner;
     // Use this for initialization
     void Start () {
@@ -15,11 +15,11 @@ public class AngleTracker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if(display1 !=null)
-        {
-           display1.text = "x: " + transform.rotation.eulerAngles.x + " y: " + transform.rotation.eulerAngles.y + " z: " + transform.rotation.eulerAngles.z;
+	   // if(display1 !=null)
+       // {
+           //display1.text = "x: " + transform.rotation.eulerAngles.x + " y: " + transform.rotation.eulerAngles.y + " z: " + transform.rotation.eulerAngles.z;
            //Debug.Log(display1.text);
-        }
+       // }
         if (transform.rotation.eulerAngles.x > 330 && tracker.isTracking)
         {
             gameObject.GetComponentInChildren<Renderer>().enabled = true;
