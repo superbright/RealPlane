9c627315e7d9e127ef6d7eab175453b85c427778
Jeff Katz <katz@kkm.berlin>
2016-10-09 16:50:42 +0200

    Add crystal assets from Caco

---
+++ Assets/_03 Scripts/BobScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BobScript : MonoBehaviour {
+	Vector3 startPosition;
+	float sin_counter = 0;
+	public float y_offset = 2f;
+	public float speed = 1f;
+	public float rotSpeed = 90f;
+
+	// Use this for initialization
+	void OnEnable () {
+		startPosition = transform.position;
+	}
+
+	public void SetInitialPosition(Vector3 pos)
+	{
+		startPosition = pos;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		sin_counter+=speed;
+		if (sin_counter > 360)
+			sin_counter = 0;
+
+		transform.position = new Vector3 (startPosition.x, startPosition.y + Mathf.Sin (sin_counter * Mathf.Deg2Rad) * y_offset, startPosition.z);
+
+		transform.RotateAround(transform.position, transform.up, Time.deltaTime * rotSpeed);
+
+
+
+
+	}
+}
---
+++ Assets/_03 Scripts/crystalSpawner.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crystalSpawner : MonoBehaviour {
+
+
+	public float radius; 
+	public float spawnTime;
+	public int maxNum;
+	public float spawn_y = 1f;
+
+	public GameObject[] Crystals;
+
+	private float lastSpawnTime;
+		
+	ArrayList spawnedObjects;
+
+	// Use this for initialization
+	void OnEnable () {
+	
+		spawnedObjects = new ArrayList();
+		lastSpawnTime = Time.realtimeSinceStartup;
+	}
+
+	void spawnSomething()
+	{
+		
+
+		/* then, instantiate a copy of the crystal */
+
+		GameObject newObj = Instantiate (Crystals[Random.Range(0, Crystals.Length - 1)]);
+		Vector3 newpos = transform.position;
+		newpos.x += Random.Range (-1 * radius, radius);
+		newpos.z += Random.Range (-1 * radius, radius);
+		newpos.y = spawn_y;
+
+		BobScript bs = newObj.GetComponent<BobScript> ();
+
+		if (bs == null) {
+			Debug.Log ("Asked to spawn some garbage");
+		} else {
+			bs.SetInitialPosition (newpos);
+		}
+
+		/* add it to my tracked list */
+		spawnedObjects.Add (newObj);
+
+		lastSpawnTime = Time.realtimeSinceStartup;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		if (Time.realtimeSinceStartup - lastSpawnTime > spawnTime)
+		{
+			/* first,go through spawned objects and remove any nulls */
+			for (int i = 0; i < spawnedObjects.Count; i++) {
+				if (spawnedObjects [i] == null) {
+					spawnedObjects.RemoveAt (i);
+				}
+			}
+
+			if (spawnedObjects.Count < maxNum) {
+				spawnSomething ();
+			}
+		}
+
+	}
+}
