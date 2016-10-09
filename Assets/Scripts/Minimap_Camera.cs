6b1fbe509d3191210f39a9e5d941491d5046a711
Jeff Katz <katz@kkm.berlin>
2016-10-09 15:51:18 +0200

    Add Minimap Camera Prefab and Render Texture
    Add Minimap Camera Prefab and Render Texture--add the prefab to a scene,
    and it will situate itself above the main camera and track it, giving
    the result to a render texture.

---
+++ Assets/Scripts/Minimap_Camera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimap_Camera : MonoBehaviour {
+    Vector3 position;
+    public float y_offset = 10f;
+    public Camera theCamera; // set me in inspector
+
+    void OnEnable () {
+        position = transform.position;
+        theCamera.farClipPlane = y_offset;
+	}
+	
+	void Update () {
+        position.x = Camera.main.transform.position.x;
+        position.z = Camera.main.transform.position.z;
+        position.y = Camera.main.transform.position.y + y_offset;
+        transform.position = position;
+	}
+}
